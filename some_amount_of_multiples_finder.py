bd10cecd15a727e6136154974892e1bf126bdccc
anirvs-coder <112902812+anirvs-coder@users.noreply.github.com>
2023-11-28 19:06:58 -0800

    more code

--- some_amount_of_multiples_finder.py
+++ some_amount_of_multiples_finder.py
@@ -1,5 +1,5 @@
 amount_of_multiples = int(input("How POSITIVE multiples do you want:"))
-number_to_find_multiples_of = int(input("What number would you like me to find " + str(amount_of_multiples) + " multiples of:"))
+number_to_find_multiples_of = int(input("What number would you like me to find " + str(amount_of_multiples) + "multiples of:"))
 numbers_list = []
 numbers = amount_of_multiples
 numbers_list.append(amount_of_multiples)
@@ -9,6 +9,7 @@ while not numbers == 0:
     numbers = numbers - 1
 numbers_list.reverse()
 multiplied_numbers_list = []
+
 for i in numbers_list:
     new_number = i * number_to_find_multiples_of
     multiplied_numbers_list.append(new_number)
@@ -16,4 +17,5 @@ for i in numbers_list:
 total = 0
 for i in range(0, len(multiplied_numbers_list)):
     total = total + multiplied_numbers_list[i]
-print(total)
+print("Here are the numbers: " + str(multiplied_numbers_list[1:len(multiplied_numbers_list)]) + ".")
+print("Here is the sum of the numbers: " + str(total) + ".")
